735ab7c06138f6349addd7612af5bcc4d7c68a0a
Efrain Quispe <EfrainQuispe156@gmail.com>
2024-09-27 00:01:36 -0500

    star Navbar-Home page

---
+++ src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../assets/logo.png"
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <img
+        src={logo}
+        class="img-fluid rounded-top"
+        alt=""
+      />
+      
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Home.css"
+import arrow from "../../assets/arrow-right.svg"
+
+function Hero() {
+  return (
+    <div className='hero container'>
+      <div className='hero-text'>
+        <h1> La cevichería preferida de los peruanos y turistas </h1>
+        <p> Somos Miramar, una cevichería y restaurante creado no sólo para ofrecerles comida marina sino para transportarlos desde que ingresan
+        a la sensación de estar en la playa.</p>
+        <button className='btn'>explore more <img src={arrow} alt="" /> </button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero
